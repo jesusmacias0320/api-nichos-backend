24317edfc4433753b8a0b9072df0c81c69f68ae2
jesusmacias0320 <jesusmacias0320@gmail.com>
2026-04-24 17:27:11 -0500

    Corrección de variable db no definida

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const db = new Pool({
   
   connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     ssl: {
